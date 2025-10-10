b25980f667a1b8426b2a4ccca81b395634a64c56
Nitesh-Sachde <202412083@daiict.ac.in>
2025-10-10 23:00:44 +0530

    Admin Dashboard UI done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
+import { AdminDashboard } from "./pages/admin/AdminDashboard";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <ThemeProvider>
+      <div className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
