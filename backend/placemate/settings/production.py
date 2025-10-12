151883640fe856a78615f02aff3c18fe4d1e1c8d
Kishan Thanki <kishanthankiat20@gmail.com>
2025-10-12 21:34:26 +0530

    fix(core): modified email-debugger

--- backend/placemate/settings/production.py
+++ backend/placemate/settings/production.py
@@ -35,7 +35,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  
     "http://127.0.0.1:5173",  
     "http://localhost:3000",   
-    "http://127.0.0.1:3000",  
+    "http://127.0.0.1:3000",
+    "http://localhost:5500",  
+    "http://127.0.0.1:5500",  
     "https://final-frontend-domain.com", 
     "https://www.final-frontend-domain.com",
 ]
@@ -46,6 +48,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000", 
     "http://localhost:5173",  
     "http://127.0.0.1:5173",  
+    "http://localhost:5500",  
+    "http://127.0.0.1:5500",  
     "https://placemate-zzgd.onrender.com", 
     "https://final-frontend-domain.com",  
 ]
