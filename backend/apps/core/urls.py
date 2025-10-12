185d474feff35a637c89a7ca608332a856d31b24
Kishan Thanki <kishanthankiat20@gmail.com>
2025-10-12 19:50:29 +0530

    feat(production): Add Temporary Email Debug

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path, include
+from .views import debug_email_config  
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -7,4 +8,5 @@ router = DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('lookup/', views.LookupAPI.as_view(), name='core-lookup'),
+    path('debug/email-config/', debug_email_config),
 ]
