51b267c2b9c971297db5938a581212ae05b3f0b7
vrushil123 <vrushilparikh123@gmail.com>
2025-10-28 18:57:23 +0530

    vite modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port : 3000,
-    proxy: {
-      '/api': {
-        target: 'https://placemate-zzgd.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      }
-    }
-  }
+    port: 3000,
+  },
 })
